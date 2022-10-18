947f1ccdc535a41bb702991030ef0dfaee9f2122
NAW2005 <sinwarnaw028@gmail.com>
2022-10-18 15:32:48 +0630

    hi

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
